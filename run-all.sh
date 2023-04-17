6c8ce4fc79e0f3d4328b1a2aa0a026e9fcc9822e
Lajos Gerecs <lajos.gerecs@erlang-solutions.com>
2023-04-17 14:22:37 +0200

    more tests

--- run-all.sh
+++ run-all.sh
@@ -6,11 +6,11 @@ export RABBITMQ_USERNAME=testuser
 export RABBITMQ_PASSWORD=rabbitmq2022
 
 tests=$(ls tests/*)
+bash -c "rebar3 compile && rebar3 release"
 for t in "${tests[@]}"; do
     test_name=$(basename $t)
     echo "Running $test_name"
     export AMCLIENT_OUT_FILE="$PWD/output/$test_name.out"
     source $t
-    bash -c "rebar3 compile && rebar3 release"
     ./_build/default/rel/amclient/bin/amclient foreground || true
 done
